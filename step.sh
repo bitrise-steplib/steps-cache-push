1023b2b7fe2ad169a89a9cf62d84914c1f3c7a8e
Viktor Benei <viktor.benei@gmail.com>
2016-03-30 18:14:32 +0200

    one more rename fix

--- step.sh
+++ step.sh
@@ -5,5 +5,5 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GO15VENDOREXPERIMENT="1"
 export GOPATH="${THIS_SCRIPT_DIR}/go"
 
-go run "${THIS_SCRIPT_DIR}/go/src/github.com/bitrise-steplib/steps-cache-upload/main.go"
+go run "${THIS_SCRIPT_DIR}/go/src/github.com/bitrise-steplib/steps-cache-push/main.go"
 exit $?
