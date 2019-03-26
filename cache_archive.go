271dc5b19e3b1d639e210094066c7141c904599f
Tamas Papik <trapacska@gmail.com>
2019-03-26 10:14:20 +0100

    do not copy file bytes if it is not regular file (no mode bits are set) (#24)
    * extra error messages
    
    * added regular file check
    
    * added extra comments and log
    
    * added FS check for CI
    
    * wired in CI check

--- cache_archive.go
+++ cache_archive.go
@@ -56,30 +56,38 @@ func NewArchive(pth string, compress bool) (*Archive, error) {
 func (a *Archive) Write(pths []string) error {
 	for _, pth := range pths {
 		info, err := os.Lstat(pth)
+		if err != nil {
+			return fmt.Errorf("failed to lstat(%s), error: %s", pth, err)
+		}
 
 		var link string
 		if info.Mode()&os.ModeSymlink != 0 {
 			link, err = os.Readlink(pth)
 			if err != nil {
-				return err
+				return fmt.Errorf("failed to read link(%s), error: %s", pth, err)
 			}
 		}
 
 		header, err := tar.FileInfoHeader(info, link)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to get tar file header(%s), error: %s", link, err)
 		}
 
 		header.Name = pth
 		header.ModTime = info.ModTime()
 
 		if err := a.tar.WriteHeader(header); err != nil {
-			return err
+			return fmt.Errorf("failed to write header(%v), error: %s", header, err)
+		}
+
+		// Calling Write on special types like TypeLink, TypeSymlink, TypeChar, TypeBlock, TypeDir, and TypeFifo returns (0, ErrWriteTooLong) regardless of what the Header.Size claims.
+		if !info.Mode().IsRegular() {
+			continue
 		}
 
 		file, err := os.Open(pth)
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to open file(%s), error: %s", pth, err)
 		}
 
 		defer func() {
@@ -88,8 +96,9 @@ func (a *Archive) Write(pths []string) error {
 			}
 		}()
 
+		// Write writes to the current file in the tar archive. Write returns the error ErrWriteTooLong if more than Header.Size bytes are written after WriteHeader.
 		if _, err := io.CopyN(a.tar, file, info.Size()); err != nil && err != io.EOF {
-			return err
+			return fmt.Errorf("failed to copy, error: %s, file: %s, size: %d for header: %v", err, file.Name(), info.Size(), header)
 		}
 	}
 
