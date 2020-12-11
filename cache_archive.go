83c9a81f8229609f2586d9f2012d722fafa773a9
Mate Herber <mate.herber@bitrise.io>
2020-12-11 21:33:42 +0100

    Revert cache-cleanup changes (#48)
    Gradle crashes when a cache file is deleted but the journal is not updated

--- cache_archive.go
+++ cache_archive.go
@@ -113,14 +113,14 @@ func (a *Archive) writeOne(pth string) error {
 	return nil
 }
 
-// WriteHeader writes the json content into the archive as a tar header.
-func (a *Archive) WriteHeader(jsonContent interface{}, pth string) error {
-	b, err := json.MarshalIndent(jsonContent, "", " ")
+// WriteHeader writes the cache descriptor file into the archive as a tar header.
+func (a *Archive) WriteHeader(descriptor map[string]string, descriptorPth string) error {
+	b, err := json.MarshalIndent(descriptor, "", " ")
 	if err != nil {
 		return err
 	}
 
-	return a.writeData(b, pth)
+	return a.writeData(b, descriptorPth)
 }
 
 // writeData writes the byte array into the archive.
